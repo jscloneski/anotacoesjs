85fcccc10f114d29b08dd31ec2b915f2b01b2a44
João Scloneski <jscloneski.dev@gmail.com>
2022-11-17 04:47:51 -0300

    att

--- anotacoes.js
+++ anotacoes.js
@@ -312,4 +312,81 @@ Assim, conseguimos fazer um comentário mais extenso*/
         techs.splice(index, 1)
 
         //console.log(techs)
+
+
+
+        Expressões e Operadores
+
+        -Expressions
+
+        ex: let number
+
+        -Operators
+            Binary
+
+            ex: console.log(number + 1)
+
+            Unary
+
+            ex: console.log(++number)
+
+            Ternary
+
+            ex: console.log(true ? 'alo' : 'nada')
+
+
+            A função new serve para criar um objeto
+
+            let name = new String('joão')
+
+
+            Operadores aritiméticos
+
+            multiplicação *
+
+            divisão /
+
+            soma +
+
+            subtração -
+
+            resto da divisão %
+            let remainder
+            remainder = 11 % 3
+            console.log(remainder)
+
+            incremento
+            let increment = 0
+            increment++
+            console.log(increment)
+
+            decremento
+            let decrement = 0
+            console.log(decrement)
+
+            exponencial **
+            console.log(3 ** 2)
+
+
+            Grouping Operator ( )
+
+            let total = (10+5) * 2
+
+            Operadores de comparação
+
+            Irá comparar valores e retornar um Boolean como resposta à comparação
+
+            let one = 1
+            let two = 2
+
+            // == igual a
+            console.log( two == 1 ) // false
+            console.log( one == "1" ) // true
+
+            != diferente de 
+            console.log( one != two ) // true
+            console.log( one != 1 ) // false
+            console.log( one != "1" ) // false
+
+
 */
